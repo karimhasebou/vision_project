fe10e6db4095129766e74dc4feba0329bbb71fdd
karimhasebou <karimhasebou@aucegypt.edu>
2017-12-13 12:16:06 +0200

    done

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
+from Tkinter import Tk
+import tkFileDialog
+from tkFileDialog import askopenfilename
 import cv2
 import numpy as np
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filenames = tkFileDialog.askopenfilenames(filetypes = (("jpeg files","*.jpg"),("all files","*.*"))) # show an "Open" dialog box and return the path to the selected file
+
+if len(filenames) < 1:
+    exit()
+
+images = [cv2.imread(x) for x in filenames]
+images_grayscale = [cv2.cvtColor(x, cv2.COLOR_BGR2GRAY) for x in images]
+
+# Find size of image1
+
+sz = images_grayscale[0].shape
+
+for idx in range(1,len(images_grayscale)):
+    warp_mode = cv2.MOTION_AFFINE
+    warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+    # Specify the number of iterations.
+    number_of_iterations = 100;
+    
+    # Specify the threshold of the increment
+    # in the correlation coefficient between two iterations
+    termination_eps = 1e-10;
+    
+    # Define termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+    
+    # Run the ECC algorithm. The results are stored in warp_matrix.
+    (cc, warp_matrix) = cv2. (images_grayscale[0], 
+        images_grayscafindTransformECCle[idx],warp_matrix, warp_mode, criteria)
+    
+    images[idx] = cv2.warpAffine(images[idx], warp_matrix, (sz[1],sz[0]), 
+        flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+
+
+images = np.stack(images, axis=3)
+print images.shape
+
+images = np.median(images, axis=3).astype(images.dtype)
+
+print images.shape
+
+cv2.namedWindow('img1', cv2.WINDOW_NORMAL)
+cv2.resizeWindow("img1", 600, 600)
+cv2.imshow("img1", images)
+cv2.waitKey(0)
+
+'''
 # Read the images to be aligned
 im1 =  cv2.imread("img1.jpg");
 im2 =  cv2.imread("img2.jpg");
@@ -58,3 +110,4 @@ cv2.imshow("img2A", im2_aligned)
 cv2.imshow("and", cv2.bitwise_and(im1, im2_aligned))
 
 cv2.waitKey(0)
+'''
